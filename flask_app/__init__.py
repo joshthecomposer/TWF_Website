74be87da53f090c15ebf67cd77079ae7039a61a1
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-03-09 14:49:23 -0500

    enable cors

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from os import environ
+from flask_cors import CORS
 
 
 application = Flask(__name__)
@@ -8,3 +9,4 @@ application.DB_HOST = environ.get("DB_HOST")
 application.DB_USER = environ.get("DB_USER")
 application.DB_PASS = environ.get("DB_PASS")
 application.DB_PORT = environ.get("DB_PORT")
+CORS(application)

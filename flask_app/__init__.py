e796232792939a7fe8d6f8ab44e7491b71ae9384
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-03-09 14:53:16 -0500

    env vars updated

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -5,8 +5,8 @@ from flask_cors import CORS
 
 application = Flask(__name__)
 application.secret_key = environ.get('SECRET_KEY')
-application.DB_HOST = environ.get("DB_HOST")
-application.DB_USER = environ.get("DB_USER")
-application.DB_PASS = environ.get("DB_PASS")
-application.DB_PORT = environ.get("DB_PORT")
+application.config["DB_HOST"] = environ.get("DB_HOST")
+application.config["DB_USER"] = environ.get("DB_USER")
+application.config["DB_PASS"] = environ.get("DB_PASS")
+application.config["DB_PORT"] = environ.get("DB_PORT")
 CORS(application)

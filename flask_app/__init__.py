59a3d8f7f85b37edde7ebb3fb1ff5c84c5ca0d5e
joshthecomposer <110321887+joshthecomposer@users.noreply.github.com>
2023-03-08 21:25:35 -0500

    remove cors

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from flask_cors import CORS
+# from flask_cors import CORS
 
 application = Flask(__name__)
 application.secret_key = 'secret key'
-CORS(application)
+# CORS(application)
